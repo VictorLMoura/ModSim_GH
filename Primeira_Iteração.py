ca47f1a4a999fdc42ad0466c3557bdeb1a926f13
VictorLMoura <victorlaperutademoura@hotmail.com>
2016-04-27 15:23:14 -0300

    Comecei

--- Primeira_Iteração.py
+++ Primeira_Iteração.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
+from numpy import linspace
 # Caso considerássemos a produção natural, levaria 24 horas para voltar à concentração
 # inicial, independente da concentração inicial. 
 # 5ng/ml por dia produzido por dia de GH.
 # V1 seria a velocidade de metabolização do GH pela enzima GHS-R.
 # dS é a taxa de variação da concentração de GH no sangue.
-V1 = 0
+V1 = 0 #Precisamos definir esse valor
+So = 0 #Definir a concentraão inicial do sangue
+Tl= linspace(0,24,1)
 def func1():
-    dS=
+    dS= -V1*So
+    return dS
+Y= odeint(func1, So, Tl)
